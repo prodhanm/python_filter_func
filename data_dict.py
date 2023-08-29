27e2ec183566cc1409b03747a0fcc8b3486a4ef7
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-29 17:15:50 -0400

    the code prints matching objects, but need to figure out how to print without the 'None' statement.

--- data_dict.py
+++ data_dict.py
@@ -1,27 +1,3 @@
-banks = {
-    "First American": {
-        "9657 American Way., \
-            Indianapolis, IN"
-    },
-    "Bank Of America": {
-        "9657 Rollins St., \
-            Milwaukee, WI"
-    },
-    "Bank of the World": {
-        "1657 Bipity Ave., \
-            New York, NY"
-    },
-    "Westworld Bank": 
-    {
-        "7878 Bopity Blvd., \
-            Hamptons, NY"
-    },
-    "Paramount Bank": {
-        "8566 Fireside Ln.,\
-            Richardson, IN"
-    },
-    "Mohminul Bank": {
-        "32-79 30th st., \
-            Astoria, NY"
-    }
-}
+names = ["Mufassa", "Layard", "Pulli", "Monet", "Bulbull", "Minhaz", \
+         "Myriad", "Mullen", "Meyer", "Walter", "Fulkrug", "Maynard", \
+            "John", "Navarro", "Mikey", "Micky", "Mousse", "Calvin"]
--- data_filt.py
+++ data_filt.py
@@ -1,8 +1,11 @@
-from data_dict import banks
+import re
+from data_dict import names
 
+def match_name():
+    for name in names:
+        pattern = re.compile("M[a-z]+")
+        data = pattern.match(name)
+        if not None:
+            print(data.groups())
 
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+match_name()
