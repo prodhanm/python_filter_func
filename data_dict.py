3756a63e2dda2f43c504ede4e1ff689658b911f6
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-29 12:46:16 -0400

    created a dictionary data and moved it to the data_dict.py file.

---
+++ data_dict.py
@@ -0,0 +1,27 @@
+banks = {
+    "First American": {
+        "9657 American Way., \
+            Indianapolis, IN"
+    },
+    "Bank Of America": {
+        "9657 Rollins St., \
+            Milwaukee, WI"
+    },
+    "Bank of the World": {
+        "1657 Bipity Ave., \
+            New York, NY"
+    },
+    "Westworld Bank": 
+    {
+        "7878 Bopity Blvd., \
+            Hamptons, NY"
+    },
+    "Paramount Bank": {
+        "8566 Fireside Ln.,\
+            Richardson, IN"
+    },
+    "Mohminul Bank": {
+        "32-79 30th st., \
+            Astoria, NY"
+    }
+}
---
+++ data_filt.py
@@ -0,0 +1,8 @@
+from data_dict import banks
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
