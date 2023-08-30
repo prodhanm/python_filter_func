787c9bc89be0192eee85788cff1d45c6b61b782f
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-30 12:39:29 -0400

    simplified the function for regexp to use the filter() to get the desired result.

--- data_filt.py
+++ data_filt.py
@@ -1,11 +1,15 @@
 import re
 from data_dict import names
 
-def match_name():
-    for name in names:
-        pattern = re.compile("M[a-z]+")
-        data = pattern.match(name)
-        if not None:
-            print(data.groups())
+def match_name(item):
+    pattern = re.compile("M[a-z]+")
+    return pattern.match(item)
 
-match_name()
+def main():
+    matched_names = list(filter(match_name,names))
+    print(f"names = {matched_names}")
+    for name in range(len(matched_names)):
+        print(matched_names[name])
+
+if __name__ == "__main__":
+    main()
