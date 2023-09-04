5dc52a43c325caa5e41216d0002a4ed841d30baf
prodhanm <mustafizur.prodhan@gmail.com>
2023-09-04 11:51:17 -0400

    tested some values with the regexp, and it works.

--- data_filt.py
+++ data_filt.py
@@ -2,14 +2,20 @@ import re
 from data_dict import names
 
 def match_name(item):
-    pattern = re.compile("M[a-z]+")
-    return pattern.match(item)
+    try:
+        pattern = re.compile("M[a-z]+")
+        return pattern.match(item)
+    except TypeError as err:
+        print(f"the variable item must be a string: {err}")
 
 def main():
-    matched_names = list(filter(match_name,names))
-    #print(f"names = {matched_names}")
-    for name in range(len(matched_names)):
-        print(matched_names[name])
+    try:
+        matched_names = list(filter(match_name,names))
+        #print(f"names = {matched_names}")
+        for name in range(len(matched_names)):
+            print(matched_names[name])
+    except ValueError as err:
+        print(err)
 
 if __name__ == "__main__":
     main()
