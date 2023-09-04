b12c6a548c7cd8dec3286477e419c2defdccb19d
prodhanm <mustafizur.prodhan@gmail.com>
2023-09-04 11:53:52 -0400

    modified the main coding file.

--- data_filt.py
+++ data_filt.py
@@ -11,7 +11,6 @@ def match_name(item):
 def main():
     try:
         matched_names = list(filter(match_name,names))
-        #print(f"names = {matched_names}")
         for name in range(len(matched_names)):
             print(matched_names[name])
     except ValueError as err:
